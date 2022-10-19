86d9b7e6e75e4bbe5ab51e977f56cb7a8d13f50e
jefri_yh <jefri_yh@yahoo.co.id>
2022-10-19 15:06:40 +0700

    add item data

--- app/src/main/java/com/sciencekom/mysecondapp/CustomerListActivity.kt
+++ app/src/main/java/com/sciencekom/mysecondapp/CustomerListActivity.kt
@@ -2,10 +2,39 @@ package com.sciencekom.mysecondapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sciencekom.mysecondapp.databinding.ActivityCustomerListBinding
 
 class CustomerListActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityCustomerListBinding
+    private val list = ArrayList<Customer>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_list)
+        binding = ActivityCustomerListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        list.addAll(listCustomers)
+
+
     }
+
+    private val listCustomers:ArrayList<Customer> get(){
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDomicile = resources.getStringArray(R.array.data_domisili)
+        val dataGender = resources.getStringArray(R.array.data_gender)
+        val dataImage = resources.obtainTypedArray(R.array.data_image)
+
+        val listCustomer = ArrayList<Customer>()
+
+        for (i in dataName.indices){
+            val customer = Customer(dataName[i],dataDomicile[i],
+                dataGender[i], dataImage.getResourceId(i, -1))
+            listCustomer.add(customer)
+        }
+
+        return listCustomer
+    }
+
+
 }
